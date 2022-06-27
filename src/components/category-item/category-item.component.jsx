f9c97ad5dd826981be5f98e89a026f17a9be7826
Talha Ahmad <talhaahmad246@yahoo.com>
2022-06-27 18:58:28 +0500

    initial routing added

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -13,7 +13,7 @@ const CategoryItem = ({ category }) => {
           <div className="category-body-container">
 
         <h2>{title}</h2>
-        <p2>Shop Now</p2>
+        <p>Shop Now</p>
         
         </div>
       </div>
